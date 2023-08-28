c0112480d30e2ef0535108c5eefb029338179f90
Akansha kaushik <128228397+akankaushik@users.noreply.github.com>
2023-08-28 11:17:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let loginCredentials = localStorage.getItem("loginStatus");
+if (loginCredentials == "false" || loginCredentials == null) {
+
+} else {
+    location.assign("./orders.html");
+}
+
+var userName = document.querySelector(".login-form-username");
+var userPassword = document.querySelector(".login-form-password");
+let submitBtn = document.getElementById("submit");
+
+let form = $("#login-form");
+form.submit(function (e) {
+    e.preventDefault();
+
+    const user = userName.value;
+    const pwd = userPassword.value;
+    var mObj = {
+        username:"akansha",
+        password:"akansha",
+    };
+    if (user === "akansha" && user !== "" && pwd !== "") {
+        console.log("yes");
+        alert("Login Successful");
+        localStorage.setItem("loginStatus", JSON.stringify(mObj));
+        location.assign("./orders.html");
+    }
+    else {
+        alert("Please Enter valid credentials!");
+    }
+})
+
